0950422db70af11db1c9a34baab5640e46ada9ad
Bartłomiej Gruchała <43046270+bgruchala@users.noreply.github.com>
2018-09-10 19:28:43 +0200

    A little bit of exercise code has been added. The course is completed.

--- intro.sql
+++ intro.sql
@@ -1,3 +1,5 @@
+-- SQL queries from excercises
+
 -- Selecting columns
 
 SELECT title FROM films;
@@ -92,13 +94,9 @@ FROM films
 WHERE release_year IN (1990, 2000)
 AND duration > 120;
 
---
-
 SELECT title, language
 FROM films
 WHERE language IN ('English', 'Spanish', 'French');
-
---
 
 SELECT title, certification
 FROM films
@@ -110,13 +108,9 @@ SELECT name
 FROM people 
 WHERE deathdate IS NULL;
 
---
-
 SELECT title 
 FROM films 
 WHERE budget IS NULL;
-
---
 
 SELECT COUNT(*) 
 FROM films 
@@ -126,11 +120,7 @@ WHERE language IS NULL;
 
 SELECT name FROM people WHERE name LIKE 'B%';
 
---
-
 SELECT name FROM people WHERE name LIKE '_r%';
-
---
 
 SELECT name FROM people WHERE name NOT LIKE 'A%';
 
@@ -172,3 +162,141 @@ WHERE release_year BETWEEN 2000 AND 2012;
 
 --
 
+SELECT title, (gross-budget) AS net_profit FROM films;
+
+SELECT title, (duration/60.0) AS duration_hours FROM films;
+
+SELECT AVG(duration)/60.0 AS avg_duration_hours FROM films;
+
+--
+
+SELECT (COUNT(deathdate)*100.0/COUNT(*)) AS percentage_dead FROM people;
+
+SELECT (MAX(release_year)-MIN(release_year)) AS difference FROM films;
+
+SELECT (MAX(release_year)-MIN(release_year))/10 AS number_of_decades FROM films;
+
+-- Sorting, grouping and joins
+
+SELECT name FROM people ORDER BY name;
+
+SELECT name FROM people ORDER BY birthdate;
+
+SELECT birthdate, name FROM people ORDER BY birthdate;
+
+--
+
+SELECT title 
+FROM films
+WHERE release_year IN (2000, 2012)
+ORDER BY release_year
+
+SELECT * 
+FROM films
+WHERE release_year <> 2015
+ORDER BY duration;
+
+SELECT title, gross 
+FROM films
+WHERE title LIKE 'M%'
+ORDER BY title;
+
+--
+
+SELECT imdb_score, film_id
+FROM reviews
+ORDER BY imdb_score DESC;
+
+SELECT title
+FROM films
+ORDER BY title DESC;
+
+SELECT title, duration
+FROM films
+ORDER BY duration DESC;
+
+--
+
+SELECT birthdate, name
+FROM people
+ORDER BY birthdate, name;
+
+SELECT release_year, duration, title
+FROM films
+ORDER BY release_year, duration;
+
+SELECT certification, release_year, title
+FROM films
+ORDER BY certification, release_year;
+
+SELECT name, birthdate
+FROM people
+ORDER BY name, birthdate;
+
+--
+
+SELECT release_year, COUNT(*)
+FROM films
+GROUP BY release_year;
+
+SELECT release_year, AVG(duration)
+FROM films
+GROUP BY release_year;
+
+SELECT release_year, MAX(budget)
+FROM films
+GROUP BY release_year;
+
+SELECT imdb_score, COUNT(*)
+FROM reviews
+GROUP BY imdb_score;
+
+--
+
+SELECT release_year, MIN(gross)
+FROM films
+GROUP BY release_year;
+
+SELECT language, SUM(gross)
+FROM films
+GROUP BY language;
+
+SELECT country, SUM(budget)
+FROM films
+GROUP BY country;
+
+SELECT release_year, country, MAX(budget)
+FROM films
+GROUP BY release_year, country
+ORDER BY release_year, country;
+
+SELECT country, release_year, MIN(gross)
+FROM films
+GROUP BY country, release_year
+ORDER BY country, release_year;
+
+--
+
+SELECT release_year, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+ORDER BY avg_gross DESC;
+
+--
+
+SELECT country, AVG(budget) AS avg_budget, AVG(gross) AS avg_gross
+FROM films
+GROUP BY country
+HAVING COUNT(country) > 10 
+ORDER BY country
+LIMIT 5
+
+--
+
+SELECT title, imdb_score
+FROM films
+JOIN reviews
+ON films.id = reviews.film_id
+WHERE title = 'To Kill a Mockingbird';
